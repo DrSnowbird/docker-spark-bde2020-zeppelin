21a60f15459208838093a2b04a8eacf306f8966a
DrSnowbird <DrSnowbird@openkbs.org>
2017-12-12 00:43:28 -0500

    update commit-push.sh Tue Dec 12 00:43:27 EST 2017

--- commit-push.sh
+++ commit-push.sh
@@ -1,24 +1,37 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Reference: https://docs.docker.com/engine/userguide/containers/dockerimages/
 
 echo "Usage: "
-echo "  ${0} <comment> <repo-name/repo-tag>"
+echo "  ${0} <comment> [<repo-name/repo-tag>] [<imageVersion>]"
+echo "e.g."
+echo "  ${0} \"initial updates\" \"openkbs/docker-project-tempalte\" \"1.0.0\" "
+echo ""
+echo "-------------------------------------"
+echo "-- Make sure you do login first:   --"
+echo "-- To login:"
+echo "       docker login"
+echo "-------------------------------------"
 echo
 
-imageTag=${2:-openkbs/docker-spark-bde2020-zeppelin}
-version=2.1.0-hadoop2.8-hive-java8
-comment=${1:-"push ${imageTag}:${version}"}
+comment=${1:-Update with the latest changes}
+
+###################################################
+#### **** Container package information ****
+###################################################
+DOCKER_IMAGE_REPO=`echo $(basename $PWD)|tr '[:upper:]' '[:lower:]'|tr "/: " "_" `
+imageTag=${2:-"openkbs/${DOCKER_IMAGE_REPO}"}
+imageVersion=${3:-"1.0.0"}
 
 docker ps -a
 
 containerID=`docker ps |grep "${imageTag} "|awk '{print $1}'`
 echo "containerID=$containerID"
 
-docker commit -m "$comment" ${containerID} ${imageTag}:${version}
-docker push ${imageTag}:${version}
+#docker tag ${imageTag} openkbs/${imageTag}:latest
 
-#docker commit -m "$comment" ${containerID} ${imageTag}:latest
-#docker push ${imageTag}:latest
+docker commit -m "$comment" ${containerID} ${imageTag}:latest
+docker push ${imageTag}:latest
 
-
+docker commit -m "$comment" ${containerID} ${imageTag}:${imageVersion}
+docker push ${imageTag}:${imageVersion}
